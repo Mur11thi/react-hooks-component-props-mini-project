cbc8992f0613b74375f14e0249a92a254b1f5ea6
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-26 12:29:56 +0300

    submitted components mini project

---
+++ src/components/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Article (props){
+    const articleDate  = props.date ||  "January 1, 1970"
+    return(
+         <article>
+         <h3>{props.title}</h3>
+         <small>{articleDate}</small>
+         <p>{props.preview}</p>
+         </article>
+    )
+}
+export default Article
